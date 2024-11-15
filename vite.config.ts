ffdcb53738938647a432448f26deb11f2b8aeb05
main <sfv4y1awg@gmail.com>
2024-11-15 14:55:52 -0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,7 +8,7 @@ export default defineConfig({
   resolve: {
     dedupe: ['react', 'react-dom'],
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   },
   build: {
